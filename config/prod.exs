34d56ac71c73dd60b229814eaddc711909541a30
SPrio-kt <supriyo.mahanta@kreeti.com>
2019-11-19 12:46:05 +0530

    config updated

--- config/prod.exs
+++ config/prod.exs
@@ -15,6 +15,7 @@ config :heroku_test, HerokuTestWeb.Endpoint,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+
 # Do not print debug messages in production
 config :logger, level: :info
 
@@ -23,6 +24,7 @@ config :heroku_test, HerokuTest.Repo,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true,
   url: System.get_env("DATABASE_URL")
+
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
@@ -59,4 +61,4 @@ config :heroku_test, HerokuTest.Repo,
 
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
-# import_config "prod.secret.exs"
+import_config "prod.secret.exs"
